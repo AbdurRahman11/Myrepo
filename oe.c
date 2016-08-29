1a5eed2dbfb902b6efb5e94b618137221145568e
rahi <mailtorahman.s@gmail.com>
2016-08-30 01:52:37 +0530

    odd or even in c

---
+++ oe.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n enter the number ");
+scanf("%d",&n);
+if((n%2)==0)
+{
+printf("\n The number is Even");
+}
+else
+{
+printf("\n The number is odd");
+}
+}
