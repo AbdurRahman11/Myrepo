b773a09f20087299e2d3d54b92f165d698f594a4
rahi <mailtorahman.s@gmail.com>
2016-08-30 01:29:43 +0530

    my first commit

---
+++ pos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n>=0)
+{
+if (n==0)
+{
+printf("\n the given number is zero");
+}
+else
+ { 
+ printf("\n the given number is Positive");
+ }
+ }else
+{ 
+ printf("\n the given number is Negative");
+ }
+ }
