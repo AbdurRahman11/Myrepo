be19c9d39de7a41edb39d81941d46209682afcaa
rahi <mailtorahman.s@gmail.com>
2016-08-30 15:46:24 +0530

    Hello Loop in java

---
+++ HelloLoop.java
@@ -0,0 +1,14 @@
+package Github;
+
+public class HelloLoop {
+	public static void main(String args[])
+	{
+		String s = "Hello";
+		int i;
+		for (i=1;i<=5;i++)
+		{
+			System.out.println(s);
+		}
+	}
+
+}
