abb0889ba092b93aab022583ca651720e2b38bf4
rahi <mailtorahman.s@gmail.com>
2016-08-30 08:42:40 +0530

    vowel or consonant

---
+++ vow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+char c;
+int a,b;
+printf("\n enter the Charecter");
+scanf("%c",&c);
+if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+{
+printf("\n it is a vowel");
+}
+else 
+{
+printf("\n it is a Consonant");
+}
+}
