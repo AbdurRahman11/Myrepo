3ebc7481bbd9fd7efd8c9cff355f87679bf1a230
rahi <mailtorahman.s@gmail.com>
2016-08-30 22:49:31 +0530

    quit on q

---
+++ q.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char c;
+    for(;;) 
+       { 
+	   printf( "\nPress Q to quit" );
+        scanf("%c", &c);
+        if ((c == 'Q')||(c=='q'))
+          
+		   break;
+			}
+    }
+  
+
